821abccd61d4ae57c908ec3620763f7a1f7e09b0
Glutanimate <glutanimate@users.noreply.github.com>
2020-01-11 15:24:36 +0100

    No need for manual add-on conflicts check, all handled by Anki

--- src/hierarchical_tags/__init__.py
+++ src/hierarchical_tags/__init__.py
@@ -18,46 +18,19 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import os
-
-from aqt import mw
 from aqt.browser import Browser
-from aqt.utils import askUser, showInfo
 
 from anki.hooks import addHook, wrap
 
 from .browser import userTagTree, onTagClick, userTagTreeOld, onTagClickOld
 from .consts import old_anki
 
-
-def uninstallHierarchicalTags(htags_path=None):
-    mw.addonManager.deleteAddon("1835859645")
-    showInfo("Uninstall successful. Please restart Anki.")
-
-
 def setupAddon():
-    name = "1835859645"
-    htags_path = os.path.join(mw.addonManager.addonsFolder(), name)
-
-    if os.path.exists(htags_path):
-        choice = askUser(
-            "It seems like you still have an old version of the "
-            "<i>Hierarchical Tags</i> add-on installed. "
-            "In order for <i>Hierarchical Tags 2</i> to work properly it is "
-            "important that you remove that version first."
-            "<br><br><b>Would you like Anki to do this for you now?</b>",
-            title="Hierarchical Tags 2 Installation",
-            parent=mw,
-        )
-        if choice:
-            uninstallHierarchicalTags(htags_path)
-
     if not old_anki:
         Browser.onTagClick = onTagClick
         Browser._userTagTree = wrap(Browser._userTagTree, userTagTree, "around")
     else:
         Browser.onTagClick = onTagClickOld
         Browser._userTagTree = wrap(Browser._userTagTree, userTagTreeOld, "around")
-
 
 addHook("profileLoaded", setupAddon)
