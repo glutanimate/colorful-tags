bcd505dd6e80e10534ca3a76413d83aded72d9f2
Glutanimate <glutanimate@users.noreply.github.com>
2021-08-26 10:38:12 +0200

    Add copyright headers

--- src/colorful_tags/sidebar/__init__.py
+++ src/colorful_tags/sidebar/__init__.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Colorful Tags for Anki
+#
+# Copyright (C) 2018-2021  Aristotelis P. <https//glutanimate.com/>
+# Copyright (C) 2021  RumovZ <gp5glkw78@relay.firefox.com>
+# Coypright (C) 2014  Patrice Neff <http://patrice.ch/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from .item import add_sidebar_item_child
 from .menu import maybe_add_context_actions
 from .model import model_data
