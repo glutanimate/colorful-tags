b98caeda322f483cacf9b685100aa73e0e17663a
Glutanimate <glutanimate@users.noreply.github.com>
2021-09-02 03:07:39 +0200

    Add a migration routine for BetterTags config

--- src/colorful_tags/data.py
+++ src/colorful_tags/data.py
@@ -30,10 +30,30 @@ assert mw is not None and mw.pm is not None
 ALL_ADDONS_PATH = Path(mw.pm.addonFolder())
 ADDON_PATH = ALL_ADDONS_PATH / __package__
 DATA_PATH = ADDON_PATH / "user_files" / "data.json"
-
+BETTERTAGS_DATA_PATH = ALL_ADDONS_PATH / "bettertags" / "user_files" / "data.json"
 
 _data: Dict[str, Any] = {}
 _tag_data: Dict[str, Any] = _data.get("tags", {})
+
+
+def _migrate_from_bettertags(data_path: Path, new_data: Dict[str, Any]):
+    with data_path.open() as data_file:
+        bettertags_data = json.load(data_file)
+    bettertags_tag_state_data = bettertags_data.get("tagState", {})
+
+    for tag, state in bettertags_tag_state_data.items():
+        pinned_state = state.get("pinned")
+        color_state = state.get("color")
+
+        if pinned_state is None and color_state is None:
+            continue
+
+        if tag not in _tag_data:
+            new_data[tag] = {}
+        if pinned_state == 1:
+            new_data[tag]["pin"] = True
+        if color_state:
+            new_data[tag]["color"] = color_state
 
 
 def tag_data() -> Dict[str, Any]:
